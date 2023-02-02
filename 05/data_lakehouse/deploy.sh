473583bbabded383f1a0810094075f48ec731180
alod83 <angelica.loduca@iit.cnr.it>
2023-02-02 12:10:48 +0100

    Added a part of chapter 5

---
+++ 05/data_lakehouse/deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+function show_help(){
+    echo "-d deploy cluster
+    -r remove cluster
+    -b delete cluster and then deploy cluster"; 
+    exit 1; 
+
+}
+
+function remove_cluster() {
+    kubectl delete rc minio --namespace presto 
+    kubectl delete rc mc --namespace presto 
+    kubectl delete svc minio --namespace presto
+    kubectl delete secret minio-secrets --namespace presto
+    kubectl delete rc spark-master --namespace presto 
+    kubectl delete svc spark-master --namespace presto 
+    kubectl delete rc,svc hive-metastore --namespace presto 
+    kubectl delete deployment,svc mysql-metastore --namespace presto 
+    #kubectl delete rc,svc presto-coordinator --namespace presto 
+    
+}
+
+function deploy_cluster(){
+    kubectl create -f minio-secrets.yaml --namespace presto
+    kubectl apply -f minio.yaml --namespace presto 
+    kubectl apply -f mysql-metastore.yaml --namespace presto
+    sleep 10
+    kubectl apply -f spark.yaml --namespace presto 
+    kubectl apply -f hive-metastore.yaml --namespace presto
+    #kubectl apply -f minio-mc.yaml --namespace presto 
+    sleep 10
+    kubectl apply -f mc.yaml --namespace presto 
+    
+    
+}
+
+# selecting mode
+while getopts "h?rdb" opt; do
+  case "$opt" in
+    h|\?)
+      show_help
+      exit 0
+      ;;
+    d) deploy_cluster
+      ;;
+    r) remove_cluster
+      ;;
+    b) remove_cluster
+       deploy_cluster
+      ;;
+  esac
+done
+
+kubectl get pods -n presto
---
+++ 05/data_lakehouse/scripts/hudi-customers-create.scala
@@ -0,0 +1,28 @@
+// spark-shell
+import org.apache.hudi.QuickstartUtils._
+import scala.collection.JavaConversions._
+import org.apache.spark.sql.SaveMode._
+import org.apache.hudi.DataSourceReadOptions._
+import org.apache.hudi.DataSourceWriteOptions._
+import org.apache.hudi.config.HoodieWriteConfig._
+import org.apache.spark.sql.functions.monotonicallyIncreasingId
+
+
+val tableName = "customers"
+val basePath = "s3a://warehouse/tables/customers"
+
+val df = spark.read.options(Map("header" -> "true")).csv("s3a://warehouse/data/customers.csv")
+
+val df2 = df.withColumn("uuid", monotonicallyIncreasingId)
+df2.write.format("hudi").
+    options(getQuickstartWriteConfigs).
+    option(RECORDKEY_FIELD_OPT_KEY, "id").
+    option(PRECOMBINE_FIELD_OPT_KEY, "dob").
+    option(TABLE_NAME, tableName).
+    mode(Overwrite).
+    saveAsTable("hudi_customers")
+
+spark.sql("select * from hudi_customers where first == \"Sharon\"").show
+
+
+System.exit(0)
